cfff963c5d575cfa35ba75349369b69d33b20be0
Walter Barreiro <walterbarreiro97@gmail.com>
2021-08-08 16:19:50 -0300

    Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-  	id:1,
-	name: 'Windstorm'
-  }
+	heroes = HEROES;
+	selectedHero?: Hero;
 
-  constructor() { }
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
 
